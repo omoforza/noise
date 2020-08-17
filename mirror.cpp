687964abf870975210e2b8bb16bbc92b081138d5
omoforza <marco.manfroi@gmail.com>
2020-08-17 22:14:06 +0200

    the reflect and transmit function were ill defined! I should have added from the start a &git status

--- mirror.cpp
+++ mirror.cpp
@@ -2,13 +2,13 @@
 #include"electric_field.h"
 #include<cmath>
 
-electric_field mirror::reflect(electric_field ef)
+electric_field mirror::reflect(electric_field & ef)
 {
 	ef.SetA(ef.GetA()*R*(-1.0));	
 	return ef;
 }
 
-electric_field mirror::transmit(electric_field ef)
+electric_field mirror::transmit(electric_field & ef)
 {
 	ef.SetA(ef.GetA()*T*sqrt(AT));	
 	return ef;
