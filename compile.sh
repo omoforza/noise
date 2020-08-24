b1159a8417b5c5a5c092db61553d094099b801e2
omoforza <marco.manfroi@gmail.com>
2020-08-24 17:13:15 +0200

    updated compile.sh files. new class names are used

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -Wall -o test.x test.cpp laser.cpp mirror.cpp cavity.cpp electric_field.cpp a.h b.h c.h f.h amplifier.h
+g++ -Wall -o test.x test.cpp laser.cpp mirror.cpp cavity.cpp electric_field.cpp AmpA.cpp AmpB.cpp AmpC.cpp AmpF.cpp amplifier.cpp
