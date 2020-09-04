149278ff0b36984acce46fa1c7156bc1d5684da4
omoforza <marco.manfroi@gmail.com>
2020-09-04 14:34:31 +0200

    Implementation of PDH stabilization apparatus. First implementation and basic testing

--- test.cpp
+++ test.cpp
@@ -2,14 +2,17 @@
 #include"mirror.h"
 #include"electric_field.h"
 #include"laser.h"
+#include"hpf.h"
 #include"amplifier.h"
 #include<iostream>
+#include<iomanip>
 #include<cmath>
 #include<fstream>
 
 using std::endl;
 using std::cout;
 using std::ofstream;
+using std::setprecision;
 
 int main()
 {
@@ -28,36 +31,48 @@ mirror m2{0.99, PI, 0.0, 0.0, sqrt(1.0-0.99*0.99), 0.0, 1.1};
 //creazione della cavità
 cavity cav{m1,m2};
 cav.AssignLaser(las);
+hpf h1;
+//attuatore piezoelettrico laser
+AmpA pz{50.0,1.0e-9};
 ofstream out;
 out.open("data.txt");
+out << setprecision(16);
 
 
 double time;
 double dt;
 double intensity;
-double amp_int;
+double temp = 0.0;
 electric_field ef;
 
 double fsr = C/(2.0*cav.GetL());
 double fres = round(las.GetFreq() / fsr)*fsr;
 double freq0 = las.GetFreq();
 
-las.SetFreq(freq0);
+las.SetFreq(fres);
 
 amplifier Ampl{600.0e3};
-for(int i=0; i<1000; i++)
+for(int i=0; i<1000000; i++)
 {
 	cav.GetNewEF(las);
 	ef = cav.GetErefl();
-	intensity = ef.Intensity();
 	time = cav.GetTime();
 	dt = cav.GetDT();
-	amp_int = Ampl.Amp(intensity, dt);
-	out << time << "\t" << intensity << "\t" << amp_int << endl;
+	out << time << "\t";
+	intensity = ef.Intensity();
+	out << intensity << "\t";
+	intensity = ef.Intensity();
+	temp = h1.filter(intensity,dt);
+	temp = temp*sin(las.GetOmegaM()*time + PI*0.5);
+	temp = Ampl.ampID(temp, dt);
+	out << temp << "\t";
+	temp = temp*10.0e3;
+	las.ErrSig(temp);
+	out << temp << endl;
+
+
 }	
 out.close();
-
-cav.rampa(las);
 
 return 0;
 }
