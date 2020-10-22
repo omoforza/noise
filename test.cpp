315cf495357275b94922a3e3493c971cfd42ba29
omoforza <marco.manfroi@gmail.com>
2020-10-22 16:20:00 +0200

    testing

--- test.cpp
+++ test.cpp
@@ -6,12 +6,12 @@ int main()
 pdh p;
 //da 500 a 10000 (oscilla)
 //p.ReflIntDynamic(20000.0e6);
-p.ReflIntDynamicB(500.0e6);
+//p.ReflIntDynamicB(500.0e6);
 
 //p.ReflIntStatic();
 
 //if true the cavity is charged at resonance
-//p.ChargeCavity(true);
+p.ChargeCavity(true);
 
 //p.Sim(true);
 //p.ErrorSignal(1500.0e6);
