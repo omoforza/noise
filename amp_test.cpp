56eff52044581170913dbde0c5de66a45e6bdcdb
omoforza <marco.manfroi@gmail.com>
2020-10-26 11:07:42 +0100

    Amp test update + amp test compile.sh

--- amp_test.cpp
+++ amp_test.cpp
@@ -1,27 +1,51 @@
 #include<iostream>
 #include<fstream>
 #include<math.h>
+#include<stdlib.h>
 #include<complex>
 #include<fftw3.h>
-#include<AmpA.h>
+#include"AmpA.h"
+#include"AmpB.h"
+#include"AmpC.h"
+#include"AmpF.h"
 
 using namespace std;
 
 int main()
 {
+	srand (5545218);
 
-//number of samples (even number!)
-int N = 100000;
+	AmpC amp;
 
-fftwl_complex* out;
+	//number of samples (even number!)
+	int N = 100000;
+	long double * input = new long double[N];
+	long double dt = 1.0e-5L;//3.0e-9
+	for(int i=0; i<N; i++)
+	{
+		input[i] = amp.ampID(rand()/double(RAND_MAX)-0.5L, dt);	
+	}
 
-out = (fftwl_complex *) fftwl_malloc(sizeof(fftwl_complex)*N);
+	
+	fftwl_complex* output;
+	
+	output = (fftwl_complex *)fftwl_malloc(sizeof(fftwl_complex)*N/2);
+	
+	fftwl_plan plan;
+	
+	plan = fftwl_plan_dft_r2c_1d(N,input,output,FFTW_ESTIMATE);
+	
+	fftwl_execute(plan);
+	
+	ofstream out;
+	out.open("amp_test.txt");
 
-fftwl_plan plan;
+	for(int i=0; i<N/2; i++){
+		out << (output[i][0]*output[i][0] + 
+			output[i][1]*output[i][1])<< endl;	
+	}
+	out.close();
 
-plan = fftwl_plan_dft_r2c_1d(N,real_data,out,FFTW_ESTIMATE);
-
-fftwl_execute(plan);
 
 return 0;
 }
---
+++ amp_test_compile.sh
@@ -0,0 +1,1 @@
+g++ -Wall -o ampTEST.x amp_test.cpp AmpA.cpp AmpB.cpp AmpC.cpp AmpF.cpp -lm -lfftw3l
