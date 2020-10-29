c9cd4fceab25f3120aa5531a6447f98541127eed
omoforza <marco.manfroi@gmail.com>
2020-10-29 16:06:21 +0100

    Start testing Reflected Intenssity Dynamic

--- test.cpp
+++ test.cpp
@@ -6,11 +6,11 @@ int main()
 pdh p;
 
 //if true the cavity is charged at resonance
-p.ChargeCavity(true);
+//p.ChargeCavity(true);
 
 
 //da 500 a 10000 (oscilla)
-//p.ReflIntDynamic(500.0e6);
+p.ReflIntDynamic(500.0e6);
 //p.ReflIntStatic();
 
 
