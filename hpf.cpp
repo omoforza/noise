e680f023ba77bbd68590df91e0d704ee874d12e4
omoforza <marco.manfroi@gmail.com>
2021-02-09 16:50:16 +0100

    new hpf function

--- hpf.cpp
+++ hpf.cpp
@@ -3,7 +3,7 @@
 long double hpf::filter(long double Iin, long double dt)
 {
 	long double Vout;
-	Vout = (V_old + R*(Iin - I_old))/(1.0L + dt*R/L )/R; 
+	Vout = (V_old + R*(Iin - I_old))/(L + dt*R ); 
 	V_old = Vout;
 	I_old = Iin;
 	return Vout;
